447982d6f52ab61132321a3e78f87fc5a900ffee
Navdeep Singh <navisahiincedo@gmail.com>
2019-05-14 20:22:02 +0000

    modifying output files

--- output.tf
+++ output.tf
@@ -3,3 +3,29 @@
 output "Bucket Name" {
   value = "${module.storage.bucketname}"
 }
+
+
+#----Networking Output -------
+
+output "public_subnets" {
+  value = "${join(", ", module.networking.public_subnets)}"
+}
+
+output "subnet_ips" {
+  value = "${join(", ", module.networking.subnet_ips)}"
+}
+
+output "Public Security Groups" {
+  value = "${module.networking.public_sg}"
+}
+
+
+#----Compute Outputs
+
+output "Public Instance IDs" {
+  value = "${module.compute.server_id}"
+}
+
+output "Public Instance IPs" {
+  value = "${module.compute.server_ip}"
+}
